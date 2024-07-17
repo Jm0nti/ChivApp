57784aa0f4cb5cb5baa80943b062a7cea3e6e0e6
Juan Pablo C <jcastanour@unal.edu.co>
2024-07-17 12:04:03 -0500

    Mas viajes y confirmacion pago

--- build/js/home.js
+++ build/js/home.js
@@ -1,152 +1,169 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const formularioViaje = document.querySelector('.formulario-viaje');
-    const contenedorViajes = document.querySelector('.contenedor-viajes');
-    const contenedorForm = document.querySelector('.contenido-home');
-    const contenedorBienvenida = document.querySelector('.bienvenida');
-    const nombreUsuario = localStorage.getItem('nombreUsuario');
-    const adminUsuario = localStorage.getItem('adminUsuario');
-    const mensajeBienvenida = document.createElement('h3');
-    
-    
-    mensajeBienvenida.textContent = `Bienvenid@ ${nombreUsuario} :), eres ${adminUsuario==="true" ? 'administrador' : 'usuario'}`;
-    contenedorBienvenida.appendChild(mensajeBienvenida);
+document.addEventListener("DOMContentLoaded", function () {
+  const formularioViaje = document.querySelector(".formulario-viaje");
+  const contenedorViajes = document.querySelector(".contenedor-viajes");
+  const contenedorForm = document.querySelector(".contenido-home");
+  const contenedorBienvenida = document.querySelector(".bienvenida");
+  const nombreUsuario = localStorage.getItem("nombreUsuario");
+  const adminUsuario = localStorage.getItem("adminUsuario");
+  const mensajeBienvenida = document.createElement("h3");
 
-    formularioViaje.addEventListener('submit', function(e) {
-        e.preventDefault();
-        
-        const origen = document.getElementById('origen').value;
-        const destino = document.getElementById('destino').value;
-        const fecha = document.getElementById('fecha').value;
-        const horaSeleccionada = document.getElementById('hora').value;
-        const n_pasajeros = document.getElementById('n_pasajeros').value;
+  mensajeBienvenida.textContent = `Bienvenid@ ${nombreUsuario} :), eres ${
+    adminUsuario === "true" ? "administrador" : "usuario"
+  }`;
+  contenedorBienvenida.appendChild(mensajeBienvenida);
 
-        if (origen === '' || destino === '' || fecha === '' || horaSeleccionada === '' || n_pasajeros === '') {
-            mostrarAlerta('Todos los campos son obligatorios', true);
-        } else if (n_pasajeros>10) {
-            mostrarAlerta('Puedes comprar como máximo 10 tickets', true);
-        } else {
-            mostrarViajes(origen, destino, fecha, horaSeleccionada, n_pasajeros);
-        }
-    });
+  formularioViaje.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    function mostrarAlerta(mensaje, error = null) {
-        // Elimina alerta si ya existe
-        const alertaExistente = document.querySelector('.alerta');
-        if (alertaExistente) {
-            alertaExistente.remove();
-        }
-        
-        // Crea alerta
-        const alerta = document.createElement("P");
-        alerta.textContent = mensaje;
-        alerta.classList.add('alerta');
-        
-        if (error) {
-            alerta.classList.add("error");
-        } else {
-            alerta.classList.add("correcto");
-        }
-        
-        contenedorForm.appendChild(alerta);
+    const origen = document.getElementById("origen").value;
+    const destino = document.getElementById("destino").value;
+    const fecha = document.getElementById("fecha").value;
+    const horaSeleccionada = document.getElementById("hora").value;
+    const n_pasajeros = document.getElementById("n_pasajeros").value;
 
-        setTimeout(() => {
-            alerta.remove();
-        }, 3000);
+    if (
+      origen === "" ||
+      destino === "" ||
+      fecha === "" ||
+      horaSeleccionada === "" ||
+      n_pasajeros === ""
+    ) {
+      mostrarAlerta("Todos los campos son obligatorios", true);
+    } else if (n_pasajeros > 10) {
+      mostrarAlerta("Puedes comprar como máximo 10 tickets", true);
+    } else {
+      mostrarViajes(origen, destino, fecha, horaSeleccionada, n_pasajeros);
+    }
+  });
+
+  function mostrarAlerta(mensaje, error = null) {
+    // Elimina alerta si ya existe
+    const alertaExistente = document.querySelector(".alerta");
+    if (alertaExistente) {
+      alertaExistente.remove();
     }
 
+    // Crea alerta
+    const alerta = document.createElement("P");
+    alerta.textContent = mensaje;
+    alerta.classList.add("alerta");
 
-    function mostrarViajes(origen, destino, fecha, horaSeleccionada, n_pasajeros) {
-        const url = 'viajes.json';
+    if (error) {
+      alerta.classList.add("error");
+    } else {
+      alerta.classList.add("correcto");
+    }
 
-        console.log('mostrando viajes');
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const viajes = data.viajes;
+    contenedorForm.appendChild(alerta);
 
-                // Limpiar contenedor de viajes previo
-                contenedorViajes.innerHTML = '';
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
 
-                // Filtrar viajes según los criterios
-                const viajesFiltrados = viajes.filter(viaje => viaje.origen === origen && viaje.destino === destino);
+  function mostrarViajes(
+    origen,
+    destino,
+    fecha,
+    horaSeleccionada,
+    n_pasajeros
+  ) {
+    const url = "viajes.json";
 
-                if (viajesFiltrados.length > 0) {
-                    viajesFiltrados.forEach(viaje => {
-                        const { precio } = viaje;
-                        const totalViaje = precio * n_pasajeros;
+    console.log("mostrando viajes");
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const viajes = data.viajes;
 
-                        const viajeHTML = document.createElement('DIV');
-                        viajeHTML.classList.add('viaje');
+        // Limpiar contenedor de viajes previo
+        contenedorViajes.innerHTML = "";
 
-                        viajeHTML.innerHTML = `
+        // Filtrar viajes según los criterios
+        const viajesFiltrados = viajes.filter(
+          (viaje) => viaje.origen === origen && viaje.destino === destino
+        );
+
+        if (viajesFiltrados.length > 0) {
+          viajesFiltrados.forEach((viaje) => {
+            const { precio } = viaje;
+            const totalViaje = precio * n_pasajeros;
+
+            const viajeHTML = document.createElement("DIV");
+            viajeHTML.classList.add("viaje");
+
+            viajeHTML.innerHTML = `
                             <h2>Detalles de viaje</h2>
                             <p><span>Origen:</span> ${origen} | <span>Destino:</span> ${destino}</p>
                             <p><span>Fecha:</span> ${fecha} | <span>Hora:</span> ${horaSeleccionada}</p>
                             <p><span>Número de pasajeros:</span> ${n_pasajeros} | <span>Precio:</span> $${totalViaje}</p>
                         `;
 
-                        contenedorViajes.appendChild(viajeHTML);
+            contenedorViajes.appendChild(viajeHTML);
 
-                        const containerBtnPagar = document.createElement('DIV');
-                        containerBtnPagar.classList.add('contenedor-btn-pagar');
-                        
-                        const botonPagar = document.createElement('BUTTON');
-                        botonPagar.textContent = 'Proceder al pago';
-                        botonPagar.classList.add('boton-pagar');
+            const containerBtnPagar = document.createElement("DIV");
+            containerBtnPagar.classList.add("contenedor-btn-pagar");
 
-                        contenedorViajes.appendChild(containerBtnPagar);
-                        containerBtnPagar.appendChild(botonPagar);
-                    });
+            const botonPagar = document.createElement("A");
+            botonPagar.textContent = "Proceder al pago";
+            botonPagar.classList.add("boton-pagar");
+            botonPagar.href = "payment.html"; // Establece el enlace al archivo payment.html
 
-                
-                } else {
-                    const mensaje = document.createElement('P');
-                    mensaje.textContent = 'No se encontraron viajes :(';
-                    mensaje.classList.add('mensaje');
-                    contenedorViajes.appendChild(mensaje);
-                }
-            })
-            .catch(error => {
-                console.error('Error al cargar el JSON:', error);
+            botonPagar.addEventListener("click", function () {
+              localStorage.setItem("totalViaje", totalViaje);
+              localStorage.setItem("fechaviaje", fecha);
             });
-    }
+            contenedorViajes.appendChild(containerBtnPagar);
+            containerBtnPagar.appendChild(botonPagar);
+          });
+        } else {
+          const mensaje = document.createElement("P");
+          mensaje.textContent = "No se encontraron viajes :(";
+          mensaje.classList.add("mensaje");
+          contenedorViajes.appendChild(mensaje);
+        }
+      })
+      .catch((error) => {
+        console.error("Error al cargar el JSON:", error);
+      });
+  }
 
-    // Cargar opciones de origen y destino
-    cargarCiudades();
+  // Cargar opciones de origen y destino
+  cargarCiudades();
 
-    function cargarCiudades() {
-        const url = 'viajes.json';
+  function cargarCiudades() {
+    const url = "viajes.json";
 
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                const horarios = data.horarios;
-                const ciudades = data.ciudades;
-                const origenSelect = document.getElementById('origen');
-                const destinoSelect = document.getElementById('destino');
-                const horaSelect = document.getElementById('hora');
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const horarios = data.horarios;
+        const ciudades = data.ciudades;
+        const origenSelect = document.getElementById("origen");
+        const destinoSelect = document.getElementById("destino");
+        const horaSelect = document.getElementById("hora");
 
-                ciudades.forEach(ciudad => {
-                    const opcionOrigen = document.createElement('option');
-                    opcionOrigen.value = ciudad;
-                    opcionOrigen.textContent = ciudad;
-                    origenSelect.appendChild(opcionOrigen);
+        ciudades.forEach((ciudad) => {
+          const opcionOrigen = document.createElement("option");
+          opcionOrigen.value = ciudad;
+          opcionOrigen.textContent = ciudad;
+          origenSelect.appendChild(opcionOrigen);
 
-                    const opcionDestino = document.createElement('option');
-                    opcionDestino.value = ciudad;
-                    opcionDestino.textContent = ciudad;
-                    destinoSelect.appendChild(opcionDestino);
-                });
+          const opcionDestino = document.createElement("option");
+          opcionDestino.value = ciudad;
+          opcionDestino.textContent = ciudad;
+          destinoSelect.appendChild(opcionDestino);
+        });
 
-                horarios.forEach(horario => {
-                    const opcionHora = document.createElement('option');
-                    opcionHora.value = horario;
-                    opcionHora.textContent = horario;
-                    horaSelect.appendChild(opcionHora);
-                });
-            })
-            .catch(error => {
-                console.error('Error al cargar el JSON:', error);
-            });
-    }
+        horarios.forEach((horario) => {
+          const opcionHora = document.createElement("option");
+          opcionHora.value = horario;
+          opcionHora.textContent = horario;
+          horaSelect.appendChild(opcionHora);
+        });
+      })
+      .catch((error) => {
+        console.error("Error al cargar el JSON:", error);
+      });
+  }
 });
--- src/js/confirmacion.js
+++ src/js/confirmacion.js
@@ -1,49 +1,59 @@
-//confirmacion de pago
-// Función para manejar la confirmación de pago por tarjeta
-function confirmarPagoTarjeta() {
-  const esPagoAceptado = Math.random() < 0.5; // 50% de probabilidad de ser aceptado
+document.addEventListener("DOMContentLoaded", function () {
+  const totalViaje = localStorage.getItem("totalViaje");
+  const fechaviaje = localStorage.getItem("fechaviaje");
 
-  if (esPagoAceptado) {
-    document.getElementById("confirmacion-titulo").textContent =
-      "Pago Confirmado";
-    document
-      .getElementById("confirmacion-mensaje")
-      .classList.remove("confirmacion-error");
-    document.getElementById("detalles-pago").innerHTML = `
+  // Función para manejar la confirmación de pago por tarjeta
+  function confirmarPagoTarjeta() {
+    const esPagoAceptado = Math.random() < 0.5; // 50% de probabilidad de ser aceptado
+
+    if (esPagoAceptado) {
+      document.getElementById("confirmacion-titulo").textContent =
+        "Pago Confirmado";
+      document
+        .getElementById("confirmacion-mensaje")
+        .classList.remove("confirmacion-error");
+      document.getElementById("detalles-pago").innerHTML = `
         <p>Detalles del pago:</p>
-        <p>Fecha: ${new Date().toLocaleDateString()}</p>
-        <p>Monto: $100.00</p>
-        <p>Tarjeta utilizada: **** **** **** 1234</p>
+        <p>Fecha: ${fechaviaje}</p>
+        <p>Monto: $${totalViaje}</p>
+        <p>La factura fue enviada al correo.</p>
       `;
-  } else {
-    document.getElementById("confirmacion-titulo").textContent =
-      "Pago Rechazado";
-    document
-      .getElementById("confirmacion-mensaje")
-      .classList.remove("confirmacion-exito");
+    } else {
+      document.getElementById("confirmacion-titulo").textContent =
+        "Pago Rechazado";
+      document
+        .getElementById("confirmacion-mensaje")
+        .classList.remove("confirmacion-exito");
+      document.getElementById("detalles-pago").innerHTML = `
+        <p>Detalles:</p>
+        <p>Fecha: ${fechaviaje}</p>
+        <p>Monto: $${totalViaje}</p>
+        <p>El pago fue rechazado, contáctate con su banco.</p>
+      `;
+    }
   }
-}
 
-// Función para manejar la confirmación de pago por QR
-function confirmarPagoQR() {
-  // Simular envío de captura de transferencia por QR
-  document.getElementById("info-adicional").textContent =
-    "Tu solicitud de pago por QR ha sido recibida. Pronto recibirás una respuesta por correo.";
-}
+  // Función para manejar la confirmación de pago por QR
+  function confirmarPagoQR() {
+    // Simular envío de captura de transferencia por QR
+    document.getElementById("info-adicional").textContent =
+      "Tu solicitud de pago por QR ha sido recibida. Pronto recibirás una respuesta por correo.";
+  }
 
-confirmarPagoTarjeta();
+  confirmarPagoTarjeta();
 
-// Event Listeners para botones de pago
-document
-  .getElementById("pay-with-card-btn")
-  .addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-    confirmarPagoTarjeta();
-  });
+  // Event Listeners para botones de pago
+  document
+    .getElementById("pay-with-card-btn")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Evitar el envío del formulario por defecto
+      confirmarPagoTarjeta();
+    });
 
-document
-  .getElementById("pay-with-qr-btn")
-  .addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
-    confirmarPagoQR();
-  });
+  document
+    .getElementById("pay-with-qr-btn")
+    .addEventListener("click", function (event) {
+      event.preventDefault(); // Evitar el envío del formulario por defecto
+      confirmarPagoQR();
+    });
+});
