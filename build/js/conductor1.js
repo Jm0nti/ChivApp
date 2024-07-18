86c12a5f6e251ad8fa8bd52faada3048d658f37c
Felipe Velez <fevelezf@unal.edu.co>
2024-07-18 15:47:04 -0500

    ya

--- build/js/conductor1.js
+++ build/js/conductor1.js
@@ -8,8 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const buttonState = JSON.parse(localStorage.getItem('buttonState')) || { enabledButtonId: 1 };
             const enabledButtonId = buttonState.enabledButtonId;
             const viajesFiltrados = data.viajes.filter(viaje => viaje.placa === placafiltro);
-            console.log(viajesFiltrados)
-            
+            console.log(viajesFiltrados);
 
             viajesFiltrados.forEach((viaje, index) => {
                 const viajeDiv = document.createElement('div');
@@ -21,30 +20,31 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p><strong>Hora de Salida:</strong> ${viaje.Hora}</p>
                     <p><strong>Destino:</strong> ${viaje.destino}</p>
                     <p><strong>Pasajeros:</strong> ${viaje.Aforo}</p>
-                    <button class="btn-iniciar" data-id="${viaje.id}" ${viaje.id == enabledButtonId ? '' : 'disabled'}>Iniciar Viaje</button>
+                    <button class="btn-iniciar" data-id="${viaje.id}">Iniciar Viaje</button>
                 `;
 
                 viajesContainer.appendChild(viajeDiv);
             });
+
             const siguienteIdHabilitado = parseInt(enabledButtonId) + 1;
             console.log(`El próximo viaje habilitado será el ${siguienteIdHabilitado}`);
 
             document.querySelectorAll('.btn-iniciar').forEach(button => {
                 button.addEventListener('click', (event) => {
                     const viajeId = event.target.getAttribute('data-id');
+                    console.log(`Iniciando viaje con ID: ${viajeId}`);
                     window.location.href = `../../conductor2.html?id=${viajeId}`;
                 });
             });
         })
         .catch(error => console.error('Error al obtener los viajes:', error));
 
+    // Reiniciar id de los viajes
+    function resetViajes() {
+        localStorage.removeItem('buttonState'); // Elimina el estado actual de localStorage
+        location.reload(); // Recarga la página para reflejar los cambios
+    }
 
-        // Reiniciar id de los viajes
-        function resetViajes() {
-            localStorage.removeItem('buttonState'); // Elimina el estado actual de localStorage
-            location.reload(); // Recarga la página para reflejar los cambios
-        }
-    
-        // Llamada directa para reiniciar los viajes
-        //resetViajes();
+    // Llamada directa para reiniciar los viajes
+    //resetViajes();
 });
